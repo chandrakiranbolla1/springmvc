c73223fc0f903e8ca171942b0f368db4091af8f3
ChandraKiran Bolla <bollachandrak@gmail.com>
2024-12-08 19:46:07 -0600

    adding common modelchanges

--- src/main/java/springmvc/controller/ContactController.java
+++ src/main/java/springmvc/controller/ContactController.java
@@ -3,32 +3,29 @@ package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import springmvc.model.User;
 
 @Controller
 public class ContactController {
+    @ModelAttribute
+    public void commonDataForModel(Model m){
+        m.addAttribute("Header", "15 Days to Go!");
+        m.addAttribute("Desc","Srinivasa Kalyanam");
+        m.addAttribute("Nithin and Raashi khanna");
+    }
+
     @RequestMapping("/contact")
     public String showForm(){
         return "Contact";
     }
 
     @RequestMapping(path = "/processform",method = RequestMethod.POST)
-    public String handleForm(
-            @RequestParam ("email") String email,
-            @RequestParam ("UserName") String name,
-            @RequestParam ("password") String pasword, Model model
-    ){
-        User user = new User();
-        user.setEmail(email);
-        user.setUserName(name);
-        user.setPassword(pasword);
-
-        model.addAttribute("user", user);
-
-        //process the data i.e send to database
-        return "sucess";
+    public String handleForm( @ModelAttribute User user, Model model){
+        System.out.println(user);
+        return "success";
     }
+
 }
